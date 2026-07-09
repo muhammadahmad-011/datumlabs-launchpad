a5d1523ea6e65230dcb8589bb71435b672ee1ad0
Muhammad Ahmad <muhammad.ahmad@datumlabs.io>
2026-07-09 14:56:45 +0500

    python_task3

---
+++ python_task3/README.md
@@ -0,0 +1,16 @@
+what the script does:
+-load data csv file
+-data exploration(df.head(), df.shape ,df.describe(),df.dtypes,df.isnull().sum())
+-data cleaning(rename columns, fill null values, change data type)
+-summary statistics:(Row count , column stats , anomalies[missing values and duplicacte value])
+-clean version to a new CSV output file
+
+
+how to run it:
+-enviroment setup
+-run the python_task3.py
+
+
+what the input and output files are:
+input csv file : titanic(train_and_test2).csv
+output files : csv_output_file
--- python_task3/python_task3.py
+++ python_task3/python_task3.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+pd.set_option("display.max_columns",None)
+
 df = pd.read_csv("python_task3/titanic(train_and_test2).csv")
 
 print(df.head())
@@ -7,3 +9,24 @@ print(df.shape)
 print(df.describe())
 print(df.dtypes)
 print(df.isnull().sum())
+
+df = df.rename(columns={"sibsp":"sibling_spouse","Pclass":"passenger_class","2urvived":"survived","Parch":"parent_children"})
+
+df["Embarked"]=df["Embarked"].fillna(0)
+
+df["Age"]=df["Age"].astype(int)
+df["Embarked"]=df["Embarked"].astype(int)
+
+
+print(f"TOTAL ROWS: {len(df)}")
+
+print(df.describe())
+
+anomalies = df.isnull().sum().sum()
+print(f"anomalies: {anomalies}")
+
+duplicates = df.duplicated().sum()
+print(f"duplicates: {duplicates}")
+
+csv_output_file = ("cleaned titanic csv file")
+df.to_csv("csv_output_file",index=False)
